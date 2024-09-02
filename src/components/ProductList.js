0d902bc6013bfbaca2a2cf6b729d8376b49630ba
LABAALLI MAHJOUBA <97558205+hajjaba-labaalli@users.noreply.github.com>
2024-09-02 11:37:52 +0000

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -9,7 +9,7 @@ function ProductList(props) {
       // Fonction d'effet pour récupérer les produits
       const fetchProducts = async () => {
         try {
-          const response = await axios.get("http://10.241.100.192:5000/api/products");
+          const response = await axios.get("http://10.241.100.196:5000/api/products");
           const fetchedProducts = response.data;
           setProducts(fetchedProducts);
         } catch (error) {
